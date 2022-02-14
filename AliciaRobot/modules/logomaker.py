cca3b9557b12b12236e1dde7d8fa1025f16909da
VENOMxCRAZY9 <97359676+VENOMxCRAZY9@users.noreply.github.com>
2022-02-14 23:05:07 +0530

    Update logomaker.py

--- AliciaRobot/modules/logomaker.py
+++ AliciaRobot/modules/logomaker.py
@@ -31,7 +31,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByAlicia.png"
     img.save(fname2, "png")
-    await bot.send_file(event.chat_id, fname2, caption="Made By @AliciaGroup_bot")
+    await bot.send_file(event.chat_id, fname2, caption="Made By @V3NOM_MUSIC_bot")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
